edf1b8dcc19a41f7e8c9fc66168884f2f331f77f
josecscosta1977 <josecscosta1977@gmail.com>
2022-07-22 20:22:28 -0300

    jquery-ajax

---
+++ assets/js/js.js
@@ -0,0 +1,20 @@
+
+function buscaCep(){
+    var cep = document.getElementById("entrada").value;
+    var url = "https://viacep.com.br/ws/" + cep + "/json/";
+    console.log(url);
+    console.log(cep);
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response){
+            $("#cep").html(response.cep);
+            $("#logradouro").html(response.logradouro);
+            $("#complemento").html(response.complemento);
+            $("#bairro").html(response.bairro);
+            $("#localidade").html(response.localidade);
+            $("#uf").html(response.uf);
+            $("#ddd").html(response.ddd);
+        }
+    })
+}
